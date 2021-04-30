ad2808dd629f00056ea2bd10ef6d83ac5a353841
toluelemson <toluwanielemosho@gmail.com>
2021-04-30 16:23:38 +0300

    integrateD swaggerUI into spring-boot

--- src/main/java/com/persons/persons/Controller.java
+++ src/main/java/com/persons/persons/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path= "/persons")
+@RequestMapping(value= "/persons")
 public class Controller {
 
     @Autowired
--- src/main/java/com/persons/persons/PersonsApplication.java
+++ src/main/java/com/persons/persons/PersonsApplication.java
@@ -9,5 +9,4 @@ public class PersonsApplication {
     public static void main(String[] args) {
         SpringApplication.run(PersonsApplication.class, args);
     }
-
 }
