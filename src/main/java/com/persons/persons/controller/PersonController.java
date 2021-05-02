d745a8662a1f98d5d22e5fa1d86dafaca1ee9ddb
toluelemson <toluwanielemosho@gmail.com>
2021-05-02 14:31:00 +0300

    changed url path from 'persons' to 'person'

--- src/main/java/com/persons/persons/controller/PersonController.java
+++ src/main/java/com/persons/persons/controller/PersonController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value= "/persons")
+@RequestMapping(value= "/person")
 public class PersonController {
 
     @Autowired
