4d3227b3357ff13e882025b3a590a9294a40aba8
toluelemson <toluwanielemosho@gmail.com>
2021-05-02 03:20:34 +0300

    refactored how db populates data when app starts

--- src/main/java/com/persons/persons/PersonsApplication.java
+++ src/main/java/com/persons/persons/PersonsApplication.java
@@ -1,12 +1,29 @@
 package com.persons.persons;
 
+import com.persons.persons.model.Person;
+import com.persons.persons.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PersonsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PersonsApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(PersonRepository personRepository) {
+        return args -> {
+            Person siim = new Person(
+                    (long) 1,
+                    "Siim Toomingas"
+            );
+
+            personRepository.save(siim);
+
+        };
     }
 }
--- src/main/resources/schema-postgres.sql
+++
@@ -1,2 +0,0 @@
-DROP TABLE IF EXISTS persons;
-CREATE TABLE persons(id serial PRIMARY KEY, person VARCHAR(255));
